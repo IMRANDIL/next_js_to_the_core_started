be1d6033ccc5ac14b49f49f26683559f358be2c3
IMRANDIL <aliimranadil2@gmail.com>
2022-02-08 18:05:26 +0530

    yh

---
+++ pages/portfolio/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return <div>
+        <h1>The Portfolio</h1>
+    </div>;
+};
+
+export default Portfolio;
---
+++ pages/portfolio/list.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const list = () => {
+    return <div>
+        <h1>The List Page</h1>
+    </div>;
+};
+
+export default list;
