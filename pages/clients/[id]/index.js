176e6db3b6f0bc63b6c07d39184882fb2bcea69e
IMRANDIL <aliimranadil2@gmail.com>
2022-02-09 18:21:07 +0530

    yh

---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+import { useRouter } from 'next/router';
+
+const ClientProjectsPage = () => {
+    const router = useRouter()
+
+    console.log(router.query);
+
+    return (
+        <div>
+            <h1>The Projects of a Given Client</h1>
+        </div>
+    )
+}
+
+export default ClientProjectsPage
