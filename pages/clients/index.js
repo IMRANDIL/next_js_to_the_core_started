a2c72e37479dfa059f62380c393422171d6398e6
IMRANDIL <aliimranadil2@gmail.com>
2022-02-10 19:16:18 +0530

    yh

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -12,13 +12,13 @@ const ClientsPage = () => {
         <div>
             <h1>The Clients Page</h1>
             <ul>
-                <li>
-                    <Link href='/clients/ali'>Ali</Link>
-                </li>
-
-                <li>
-                    <Link href='/clients/imran'>Imran</Link>
-                </li>
+                {clients.map((client) => {
+                    return (
+                        <li key={client.id}>
+                            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
