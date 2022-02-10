772354879fdbf9dd7db22af2ef6060bbde4fccd6
IMRANDIL <aliimranadil2@gmail.com>
2022-02-10 18:58:39 +0530

    yh

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+
+import Link from "next/link"
+
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div >
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href='/portfolio'>Portfolio</Link>
+        </li>
+
+        <li>
+          <Link href='/clients'>Clients</Link>
+        </li>
+      </ul>
     </div>
   )
 }
