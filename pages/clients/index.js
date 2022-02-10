75a5eef1d865f1b5d6384c2e9f3b9f14832b265b
IMRANDIL <aliimranadil2@gmail.com>
2022-02-10 19:03:45 +0530

    yh

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const ClientsPage = () => {
+
+    const clients = [
+        { id: 'ali', name: 'Ali' },
+        { id: 'imran', name: 'Imran' }
+    ]
+
     return (
         <div>
             <h1>The Clients Page</h1>
+            <ul>
+                <li>
+                    <Link href='/clients/ali'>Ali</Link>
+                </li>
+
+                <li>
+                    <Link href='/clients/imran'>Imran</Link>
+                </li>
+            </ul>
         </div>
     )
 }
